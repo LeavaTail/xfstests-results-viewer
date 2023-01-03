2a2fe8beca51a51de6a49041c8c14c7396d89ae7
LeavaTail <starbow.duster@gmail.com>
2023-01-03 15:31:11 +0900

    test: change to pytest framework
    Signed-off-by: LeavaTail <starbow.duster@gmail.com>

--- tests/test_testcase.py
+++ tests/test_testcase.py
@@ -6,134 +6,103 @@ testcase class method verification
 """
 
 import os
-import unittest
+import pytest
 from logging import getLogger, CRITICAL
-from analyzer.testcase import PassedClass, SkippedClass, FailedClass
+from viewer.testcase import PassedClass, SkippedClass, FailedClass
 
 getLogger("__main__").setLevel(CRITICAL)
 
-class TestPassedClass(unittest.TestCase):
-    """Unit test for PassedClass
+@pytest.fixture(scope='module', autouse=True)
+def module_fixture():
+    """Prepare the xfstests results directory
 
-    PassedClass verification
-    1. constructor will set `name`
-    2. update_time() will set `sec`
-    3. update_summary() will be noop
-    4. update_path() will be `path` (concatenate basepath to filepath)
+    SkippedClass lookup *.notrun file to input the reason why the test
+    will be skipped. To verify lookup sequence, create the simple results
+    directory. (`dir/foo.notrun`)
     """
+    # Set up process
+    os.mkdir('dir')
+    with open('dir/foo.notrun', mode='x') as f:
+        f.write('sample')
 
-    def test_passed1(self):
-        target = PassedClass('foo')
-        self.assertEqual(target.name, 'foo')
+    # Main process
+    yield
 
-    def test_passed2(self):
-        target = PassedClass('foo')
-        target.update_time(100)
-        self.assertEqual(target.sec, 100)
+    # Clean up process
+    os.remove('dir/foo.notrun')
+    os.rmdir('dir')
 
-    def test_passed3(self):
-        target = PassedClass('foo')
-        target.update_summary('bar')
-        self.assertEqual(target.remarks, '')
+# PassedClass constructor will set `name`
+def test_passed1():
+    target = PassedClass('foo')
+    assert target.name == 'foo'
 
-    def test_passed4(self):
-        target = PassedClass('foo')
-        target.update_path('path')
-        self.assertEqual(target.path, 'path/foo')
+# PassedClass update_time() will set `sec`
+def test_passed2():
+    target = PassedClass('foo')
+    target.update_time(100)
+    assert target.sec == 100
 
-class TestSkippedClass(unittest.TestCase):
-    """Unit test for SkippedClass
+# PassedClass update_summary() will be noop
+def test_passed3():
+    target = PassedClass('foo')
+    target.update_summary('bar')
+    assert target.remarks == ''
 
-    SkippedClass verification
-    1. constructor will set `name`
-    2. update_time() will set `sec`
-    3. update_summary() will set `remarks` if file is exist
-    4. update_summary() will raise SystemExit if file is not exist
-    5. update_path() will be `path` (concatenate basepath to filepath)
-    """
+# PassedClass update_path() will set `path`
+def test_passed4():
+    target = PassedClass('foo')
+    target.update_path('path')
+    assert target.path == 'path/foo'
 
-    base = 'dir'
+# SkippedClass constructor will set `name`
+def test_skipped1():
+    target = SkippedClass('foo')
+    assert target.name == 'foo'
 
-    @classmethod
-    def setUpClass(cls):
-        """Prepare the xfstests results directory
+# SkippedClass update_time() will set `sec`
+def test_skipped2():
+    target = SkippedClass('foo')
+    target.update_time(100)
+    assert target.sec == 100
 
-        SkippedClass lookup *.notrun file to input the reason why the test
-        will be skipped. To verify lookup sequence, create the simple results
-        directory. (`dir/foo.notrun`)
+# SkippedClass update_summary() will set `remarks` if file is exist
+def test_skipped3():
+    target = SkippedClass('foo')
+    target.update_summary('dir/')
+    assert target.remarks == 'sample'
 
-        Note:
-            That directory and file should be removed after verification
-        """
-        os.mkdir('dir')
-        with open('dir/foo.notrun', mode='x') as f:
-            f.write('sample')
-        return super().setUpClass()
-
-    @classmethod
-    def tearDownClass(cls):
-        """Clean up the xfstests results directory
-
-        SkippedClass lookup *.notrun file to input the reason why the test
-        will be skipped in setUpClass. these file should be removed after
-        verification.
-        """
-        os.remove('dir/foo.notrun')
-        os.rmdir('dir')
-        return super().tearDownClass()
-
-    def test_skipped1(self):
-        target = SkippedClass('foo')
-        self.assertEqual(target.name, 'foo')
-
-    def test_skipped2(self):
-        target = SkippedClass('foo')
-        target.update_time(100)
-        self.assertEqual(target.sec, 100)
-
-    def test_skipped3(self):
-        target = SkippedClass('foo')
+# SkippedClass update_summary() will raise exception if file is not exist
+def test_skipped4():
+    target = SkippedClass('bar')
+    with pytest.raises(SystemExit):
         target.update_summary('dir/')
-        self.assertEqual(target.remarks, 'sample')
 
-    def test_skipped4(self):
-        target = SkippedClass('bar')
-        with self.assertRaises(SystemExit):
-            target.update_summary('dir/')
+# SkippedClass update_path() will set `path`
+def test_skipped5():
+    target = SkippedClass('foo')
+    target.update_path('dir')
+    assert target.path == 'dir/foo'
 
-    def test_skipped5(self):
-        target = SkippedClass('foo')
-        target.update_path('dir')
-        self.assertEqual(target.path, 'dir/foo')
+# FailedClass constructor will set `name`
+def test_failed1():
+    target = FailedClass('foo')
+    assert target.name == 'foo'
 
-class TestFailedClass(unittest.TestCase):
-    """Unit test for FailedClass
+# FailedClass update_time() will set `sec`
+def test_failed2():
+    target = FailedClass('foo')
+    target.update_time(100)
+    assert target.sec == 100
 
-    FailedClass verification
-    1. constructor will set `name`
-    2. update_time() will set `sec`
-    3. update_summary() will be noop
-    4. update_path() will be `path` (concatenate basepath to filepath)
-    """
+# FailedClass update_summary() will be noop
+def test_failed3():
+    target = FailedClass('foo')
+    target.update_summary('bar')
+    assert target.remarks == ''
 
-    def test_failed1(self):
-        target = FailedClass('foo')
-        self.assertEqual(target.name, 'foo')
-
-    def test_failed2(self):
-        target = FailedClass('foo')
-        target.update_time(100)
-        self.assertEqual(target.sec, 100)
-
-    def test_failed3(self):
-        target = FailedClass('foo')
-        target.update_summary('bar')
-        self.assertEqual(target.remarks, '')
-
-    def test_failed4(self):
-        target = FailedClass('foo')
-        target.update_path('path')
-        self.assertEqual(target.path, 'path/foo')
-
-if __name__ == "__main__":
-    unittest.main()
+# FailedClass update_path() will set `path`
+def test_failed4():
+    target = FailedClass('foo')
+    target.update_path('path')
+    assert target.path == 'path/foo'
