8cfdf3b7297d760fab68e10411ea0418d94c4747
LeavaTail <starbow.duster@gmail.com>
2023-01-03 20:13:18 +0900

    test: follow the I/F updates in testcase class
    Due to ecc503cba2af, the interface was changed.
    
    Signed-off-by: LeavaTail <starbow.duster@gmail.com>

--- tests/test_testcase.py
+++ tests/test_testcase.py
@@ -6,8 +6,10 @@ testcase class method verification
 """
 
 import os
+import shutil
 import pytest
 from logging import getLogger, CRITICAL
+from tests.setup_env import create_timefile, create_logfile, create_notrun
 from viewer.testcase import PassedClass, SkippedClass, FailedClass
 
 getLogger("__main__").setLevel(CRITICAL)
@@ -20,89 +22,94 @@ def module_fixture():
     will be skipped. To verify lookup sequence, create the simple results
     directory. (`dir/foo.notrun`)
     """
-    # Set up process
-    os.mkdir('dir')
-    with open('dir/foo.notrun', mode='x') as f:
-        f.write('sample')
+    param = {
+        'dir': 'dir/',
+        'pass': ['generic/001'],
+        'skip': ['generic/002'],
+        'fail': ['generic/003'],
+        'time': [10]
+    }
+    # Set up tempdir
+    os.mkdir(param['dir'])
+
+    # Create xfstest results files
+    create_timefile(param['dir'], param['pass'], param['time'])
+    create_logfile(param['dir'], param['pass'], param['skip'], param['fail'])
+    create_notrun(param['dir'], param['skip'])
 
     # Main process
-    yield
+    yield param
 
     # Clean up process
-    os.remove('dir/foo.notrun')
-    os.rmdir('dir')
+    shutil.rmtree(param['dir'])
 
-# PassedClass constructor will set `name`
-def test_passed1():
-    target = PassedClass('foo')
-    assert target.name == 'foo'
-
-# PassedClass update_time() will set `sec`
-def test_passed2():
-    target = PassedClass('foo')
-    target.update_time(100)
-    assert target.sec == 100
-
-# PassedClass update_summary() will be noop
-def test_passed3():
-    target = PassedClass('foo')
-    target.update_summary('bar')
+# PassedClass constructor will set (default)
+def test_passed1(module_fixture):
+    param = module_fixture
+    target = PassedClass(param['pass'][0])
+    assert target.name == param['pass'][0]
+    assert target.sec == 0
     assert target.remarks == ''
+    assert target.path == ''
 
-# PassedClass update_path() will set `path`
-def test_passed4():
-    target = PassedClass('foo')
-    target.update_path('path')
-    assert target.path == 'path/foo'
-
-# SkippedClass constructor will set `name`
-def test_skipped1():
-    target = SkippedClass('foo')
-    assert target.name == 'foo'
-
-# SkippedClass update_time() will set `sec`
-def test_skipped2():
-    target = SkippedClass('foo')
-    target.update_time(100)
+# PassedClass constructor will set
+def test_passed2(module_fixture):
+    param = module_fixture
+    target = PassedClass(param['pass'][0], 100, 'foo', 'bar')
+    assert target.name == param['pass'][0]
     assert target.sec == 100
+    assert target.remarks == 'foo'
+    assert target.path == 'bar'
 
-# SkippedClass update_summary() will set `remarks` if file is exist
-def test_skipped3():
-    target = SkippedClass('foo')
-    target.update_summary('dir/')
-    assert target.remarks == 'sample'
+# update_time() will set `sec`
+def test_passed3(module_fixture):
+    param = module_fixture
+    target = PassedClass(param['pass'][0])
+    target.update_time(param['dir'])
+    assert target.sec == param['time'][0]
 
-# SkippedClass update_summary() will raise exception if file is not exist
-def test_skipped4():
-    target = SkippedClass('bar')
-    with pytest.raises(SystemExit):
-        target.update_summary('dir/')
-
-# SkippedClass update_path() will set `path`
-def test_skipped5():
-    target = SkippedClass('foo')
-    target.update_path('dir')
-    assert target.path == 'dir/foo'
-
-# FailedClass constructor will set `name`
-def test_failed1():
-    target = FailedClass('foo')
-    assert target.name == 'foo'
-
-# FailedClass update_time() will set `sec`
-def test_failed2():
-    target = FailedClass('foo')
-    target.update_time(100)
-    assert target.sec == 100
-
-# FailedClass update_summary() will be noop
-def test_failed3():
-    target = FailedClass('foo')
-    target.update_summary('bar')
+# SkippedClass constructor will set (default)
+def test_skipped1(module_fixture):
+    param = module_fixture
+    target = SkippedClass(param['skip'][0])
+    assert target.name == param['skip'][0]
+    assert target.sec == 0
     assert target.remarks == ''
+    assert target.path == ''
 
-# FailedClass update_path() will set `path`
-def test_failed4():
-    target = FailedClass('foo')
-    target.update_path('path')
-    assert target.path == 'path/foo'
+# SkippedClass constructor will set
+def test_skipped2(module_fixture):
+    param = module_fixture
+    target = SkippedClass(param['skip'][0], 100, 'foo', 'bar')
+    assert target.name == param['skip'][0]
+    assert target.sec == 100
+    assert target.remarks == 'foo'
+    assert target.path == 'bar'
+
+# update_summary will set `remarks`
+def test_skipped3(module_fixture):
+    param = module_fixture
+    target = SkippedClass(param['skip'][0])
+    target.update_summary(param['dir'])
+    assert target.name == param['skip'][0]
+    assert target.sec == 0
+    assert target.remarks == param['skip'][0]
+    assert target.path == ''
+
+# FailedClass constructor will set (default)
+def test_failped1(module_fixture):
+    param = module_fixture
+    target = FailedClass(param['fail'][0])
+    assert target.name == param['fail'][0]
+    assert target.sec == 0
+    assert target.remarks == ''
+    assert target.path == ''
+
+# FailedClass constructor will set
+def test_failped2(module_fixture):
+    param = module_fixture
+    target = FailedClass(param['fail'][0], 100, 'foo', 'bar')
+    assert target.name == param['fail'][0]
+    assert target.sec == 100
+    assert target.remarks == 'foo'
+    assert target.path == 'bar'
